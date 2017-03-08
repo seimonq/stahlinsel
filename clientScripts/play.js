d92c9b7eabfdb7ef7cd49af8b86c402d0db53ba8
sikern <simon.kern@capgemini.com>
2017-03-08 17:35:09 +0100

    stateNodeedge data displayed in play.html

--- clientScripts/play.js
+++ clientScripts/play.js
@@ -32,40 +32,75 @@ function htmlDecode(value){
 function selectNode(nodeIndex) {
 		var requestData = global.helper.composeRequestTO("selectNode",nodeIndex);
 		global.helper.requestEditWithGetJson(requestData,callbackSelectPlayNode);
-	}
+}
+	
 function callbackSelectPlayNode(responseData, onSuccessMsg) {
 	
-	//alert(responseData.toSource());
+	reset_play_website();
+	alert(responseData.toSource());
 	
 	$("#focused-node-text").html(htmlDecode(responseData.text));
 	
 	$("#parent-node-box").html("");
-	responseData.parentList.forEach(function(element,key) {
-	var parentId = element.parent_id;
-		$("#parent-node-box").append('<button type="button" id="parent-'+parentId+'"\
-			class="margin-bottom btn btn-info" title="nodeIndex:'+parentId+'">'+element.relatedNodeName+'</button>\
-			Teaser: '+element.teaser+'<br><br>');
+	if(responseData.hasOwnProperty('parentList') && responseData.parentList.length) {
+		responseData.parentList.forEach(function(element,key) {
+			var parentId = element.parent_id;
+			//
+			$("#parent-node-box").append('<button type="button" id="parent-'+parentId+'"\
+				class="margin-bottom btn btn-info" title="nodeIndex:'+parentId+'">'+element.relatedNodeName+'</button>\
+				Teaser: '+element.teaser+'<br><br>');
+			//
+			displayStatesForNodeedges(element,responseData.name,"parent");
+			//event
 			$("#parent-"+parentId).click(function(e) {
 				$("#select-play-node-by-chapter option[value="+parentId+"]").
 					prop('selected', true);
 				selectNode(parentId);
 			});
 		});
+	}
 		
 	$("#child-node-box").html("");
-	responseData.childList.forEach(function(element, key) {
-	var childId = element.child_id;
-		$("#child-node-box").append('<button type="button" id="child-'+childId+'"\
-			class="margin-bottom btn btn-info" title="nodeIndex:'+childId+'">'+element.relatedNodeName+'</button>\
-			Teaser: '+element.teaser+'<br><br>');
+	if(responseData.hasOwnProperty('childList') && responseData.childList.length) {
+		responseData.childList.forEach(function(element, key) {
+			var childId = element.child_id;
+			//
+			$("#child-node-box").append('<button type="button" id="child-'+childId+'"\
+				class="margin-bottom btn btn-info" title="nodeIndex:'+childId+'">'+element.relatedNodeName+'</button>\
+				Teaser: '+element.teaser+'<br><br>');
+			//
+			displayStatesForNodeedges(element,responseData.name,"child");
+			//event
 			$("#child-"+childId).click(function() {
 				$( "#select-play-node-by-chapter option[value='"+childId+"']").
 					prop('selected', true);
 				selectNode(childId);
 			});
 		});
-		
 	}
+}
+//
+function displayStatesForNodeedges(element,focusedNodeName,relation) {
+	if(element.stateNodeedges.length > 0) {
+		$("#"+relation+"-node-info-box").append('Bedingung für '+
+			element.relatedNodeName+'<->'+focusedNodeName+' : ');
+		element.stateNodeedges.forEach(function(stateElement, stateKey) {
+			$("#"+relation+"-node-info-box").append(" <b>"+stateElement.name+"</b> ");
+		});
+	}
+}
+function reset_play_website() {
+		
+		$("#parent-node-box").html("");
+		$("#parent-node-info-box").html("");
+		
+		$("#focused-node-box").html("");
+		$("#focused-node-info-box").html("");
+
+		$("#child-node-box").html("");
+		$("#child-node-info-box").html("");
+		
+}
 
 //##############################################################################################
 //##############################################################################################
@@ -75,6 +110,8 @@ function callbackSelectPlayNode(responseData, onSuccessMsg) {
 
 window.onload = function() {
 	
+		reset_play_website();
+
 		global.chapter.getChapterIndex();
 		$("#select-play-node-by-chapter").html("<option> -- no option -- </option>");
 		
